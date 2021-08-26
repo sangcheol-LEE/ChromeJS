cce3b834bbc1e56005e810ca504075bf8fc599f0
이상철 <zlrz6706@gmail.com>
2021-08-26 17:58:46 +0900

    make username

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
+const loginForm = document.querySelector('#login-form')
 const loginInput = document.querySelector('#login-form input');
-const loginButton = document.querySelector('#login-form  button');
+const greeting = document.querySelector('#greeting');
 
-function onLoginBtnClick () {
-  console.log(loginInput.value)
+const HIDDEN_CLASSNAME = "hidden" // 문자열로 되있는 것을 오타 안나게 하려고 변수처리 할 때 대문자로 사용
+const USERNAME_KEY = "username"
+
+function onLoginSubmit (event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME)
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username)
 }
+ function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME)
+ }
 
-loginButton.addEventListener('click', onLoginBtnClick);
+const savedUsername = localStorage.getItem(USERNAME_KEY)
 
+if(savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener('submit', onLoginSubmit)
 
+}else {
+  paintGreetings(savedUsername)
+}
