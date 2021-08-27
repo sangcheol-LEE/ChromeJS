24908417c7c60ce78883dcc2130cb255d419cdb3
이상철 <zlrz6706@gmail.com>
2021-08-27 15:05:10 +0900

    add todo-list delete button

--- JS/todo.js
+++ JS/todo.js
@@ -3,11 +3,23 @@ const toDoInput = toDoForm.querySelector('input');// 1번
 //const toDoInput = document.querySelector('#todo-form input');// 2번 // 1번과 2번은 같다.
 const toDoList = document.getElementById('todo-list');
 
+
+function deleteToDo (event) {
+  const li =event.target.parentElement; // 지금 누르는 버튼이 어떤 li 태그인지 알고,
+  //  삭제할 수 있게 확인하는 방법 . 미쳤다 ,,,,
+  li.remove(); //버튼의 부모요소인 li를 버튼을 클릭하면 삭제시킵니다.
+}
+
 function paintToDo (newTodo) {
   const li = document.createElement('li');
   const span = document.createElement('span');
-  li.appendChild(span); // li는 span 이라는 자식을 갖게 되었다.
   span.innerText = newTodo;
+
+  const button = document.createElement('button');
+  button.innerText = "❌ ";
+  button.addEventListener('click',deleteToDo);
+  li.appendChild(span); // li는 span 이라는 자식을 갖게 되었다.
+  li.appendChild(button);
   toDoList.appendChild(li);
 }
 
